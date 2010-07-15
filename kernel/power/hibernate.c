bb446ffee6be582c697f30425d3256ceffbfc81c
Thomas Gleixner <tglx@linutronix.de>
2010-07-15 10:29:00 +0200

    suspend: Prevent might sleep splats
    timekeeping suspend/resume calls read_persistant_clock() which takes
    rtc_lock. That results in might sleep warnings because at that point
    we run with interrupts disabled.
    
    We cannot convert rtc_lock to a raw spinlock as that would trigger
    other might sleep warnings.
    
    As a temporary workaround we disable the might sleep warnings by
    setting system_state to SYSTEM_SUSPEND before calling sysdev_suspend()
    and restoring it to SYSTEM_RUNNING afer sysdev_resume().
    
    Needs to be revisited.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/power/hibernate.c
+++ kernel/power/hibernate.c
@@ -270,6 +270,8 @@ static int create_image(int platform_mode)
 
 	local_irq_disable();
 
+	system_state = SYSTEM_SUSPEND;
+
 	error = syscore_suspend();
 	if (error) {
 		printk(KERN_ERR "PM: Some system devices failed to power down, "
@@ -297,6 +299,7 @@ static int create_image(int platform_mode)
 	syscore_resume();
 
  Enable_irqs:
+	system_state = SYSTEM_RUNNING;
 	local_irq_enable();
 
  Enable_cpus:
@@ -422,6 +425,7 @@ static int resume_target_kernel(bool platform_mode)
 		goto Enable_cpus;
 
 	local_irq_disable();
+	system_state = SYSTEM_SUSPEND;
 
 	error = syscore_suspend();
 	if (error)
@@ -455,6 +459,7 @@ static int resume_target_kernel(bool platform_mode)
 	syscore_resume();
 
  Enable_irqs:
+	system_state = SYSTEM_RUNNING;
 	local_irq_enable();
 
  Enable_cpus:
@@ -543,6 +548,7 @@ int hibernation_platform_enter(void)
 		goto Platform_finish;
 
 	local_irq_disable();
+	system_state = SYSTEM_SUSPEND;
 	syscore_suspend();
 	if (pm_wakeup_pending()) {
 		error = -EAGAIN;
@@ -555,6 +561,7 @@ int hibernation_platform_enter(void)
 
  Power_up:
 	syscore_resume();
+	system_state = SYSTEM_RUNNING;
 	local_irq_enable();
 	enable_nonboot_cpus();
 
