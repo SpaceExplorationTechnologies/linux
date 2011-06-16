92eaf4ed4ac0ffa3b5a4626051be858e97bd0dff
Thomas Gleixner <tglx@linutronix.de>
2011-06-16 13:26:08 +0200

    sched-migrate-disable.patch
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1275,6 +1275,7 @@ struct task_struct {
 #endif
 
 	unsigned int policy;
+	int migrate_disable;
 	int nr_cpus_allowed;
 	cpumask_t cpus_allowed;
 
@@ -1613,9 +1614,6 @@ struct task_struct {
 	struct rcu_head put_rcu;
 #endif
 };
-
-/* Future-safe accessor for struct task_struct's cpus_allowed. */
-#define tsk_cpus_allowed(tsk) (&(tsk)->cpus_allowed)
 
 #ifdef CONFIG_PREEMPT_RT_FULL
 static inline bool cur_pf_disabled(void) { return current->pagefault_disabled; }
@@ -2772,6 +2770,15 @@ static inline void set_task_cpu(struct task_struct *p, unsigned int cpu)
 }
 
 #endif /* CONFIG_SMP */
+
+/* Future-safe accessor for struct task_struct's cpus_allowed. */
+static inline const struct cpumask *tsk_cpus_allowed(struct task_struct *p)
+{
+	if (p->migrate_disable)
+		return cpumask_of(task_cpu(p));
+
+	return &p->cpus_allowed;
+}
 
 extern long sched_setaffinity(pid_t pid, const struct cpumask *new_mask);
 extern long sched_getaffinity(pid_t pid, struct cpumask *mask);
