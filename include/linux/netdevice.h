d56f5950db46401f7f7ee0f850a2bd73efdfac19
Thomas Gleixner <tglx@linutronix.de>
2011-07-12 15:38:34 +0200

    skbufhead-raw-lock.patch
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1783,6 +1783,7 @@ struct softnet_data {
 	unsigned int		dropped;
 	struct sk_buff_head	input_pkt_queue;
 	struct napi_struct	backlog;
+	struct sk_buff_head	tofree_queue;
 };
 
 static inline void input_queue_head_incr(struct softnet_data *sd)
