f7f2086c075a5dc154570e21dd57795fd967e934
Thomas Gleixner <tglx@linutronix.de>
2011-07-18 13:59:17 +0200

    softirq-disable-softirq-stacks-for-rt.patch
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/powerpc/kernel/irq.c
+++ arch/powerpc/kernel/irq.c
@@ -538,6 +538,7 @@ void irq_ctx_init(void)
 	}
 }
 
+#ifndef CONFIG_PREEMPT_RT_FULL
 static inline void do_softirq_onstack(void)
 {
 	struct thread_info *curtp, *irqtp;
@@ -574,6 +575,7 @@ void do_softirq(void)
 
 	local_irq_restore(flags);
 }
+#endif
 
 irq_hw_number_t virq_to_hw(unsigned int virq)
 {
