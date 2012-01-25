7dddb43fdec9851f956328024e2adb07734599f8
Thomas Gleixner <tglx@linutronix.de>
2012-01-25 11:08:40 +0100

    timer-fd: Prevent live lock
    If hrtimer_try_to_cancel() requires a retry, then depending on the
    priority setting te retry loop might prevent timer callback completion
    on RT. Prevent that by waiting for completion on RT, no change for a
    non RT kernel.
    
    Reported-by: Sankara Muthukrishnan <sankara.m@gmail.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: stable-rt@vger.kernel.org

--- fs/timerfd.c
+++ fs/timerfd.c
@@ -324,7 +324,7 @@ SYSCALL_DEFINE4(timerfd_settime, int, ufd, int, flags,
 		if (hrtimer_try_to_cancel(&ctx->tmr) >= 0)
 			break;
 		spin_unlock_irq(&ctx->wqh.lock);
-		cpu_relax();
+		hrtimer_wait_for_timer(&ctx->tmr);
 	}
 
 	/*
