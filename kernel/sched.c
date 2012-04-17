8d5b7e9adf464f4923d4097fba4bcb2c8ec038d2
Steven Rostedt <srostedt@redhat.com>
2012-04-16 21:51:54 -0400

    rt: Make migrate_disable/enable() and __rt_mutex_init non-GPL only
    Modules that load on the normal vanilla kernel should also load on
    an -rt kernel as well. This does not mean we condone non-GPL modules,
    we are only being consistent.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- kernel/sched.c
+++ kernel/sched.c
@@ -4252,7 +4252,7 @@ void migrate_disable(void)
 	p->migrate_disable = 1;
 	preempt_enable();
 }
-EXPORT_SYMBOL_GPL(migrate_disable);
+EXPORT_SYMBOL(migrate_disable);
 
 void migrate_enable(void)
 {
@@ -4304,7 +4304,7 @@ void migrate_enable(void)
 	unpin_current_cpu();
 	preempt_enable();
 }
-EXPORT_SYMBOL_GPL(migrate_enable);
+EXPORT_SYMBOL(migrate_enable);
 #else
 static inline void update_migrate_disable(struct task_struct *p) { }
 #define migrate_disabled_updated(p)		0
